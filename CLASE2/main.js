5ede4a7ea2c9b64a019e3a6c3f7ee42871c88abf
lucasluccaroni <lucas_luccaroni@hotmail.com.ar>
2023-11-20 17:54:51 -0300

    actualizado notas clase 2

---
+++ CLASE2/main.js
@@ -0,0 +1,14 @@
+// CLASE 2 - CREACION Y CONFIGURACION DEL PROYECTO
+/*
+
+USANDO VITE
+1) npm create vite@lastest
+2) seguir los pasos -> nombre de proyecto -> React -> Javascript -> cd nombre-proyecto -> npm istall -> npm run dev
+
+
+
+
+
+
+
+*/
