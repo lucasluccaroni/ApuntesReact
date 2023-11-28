bd385662406c4a975e6634d8fb80db6b55576671
lucasluccaroni <lucas_luccaroni@hotmail.com.ar>
2023-11-27 22:51:58 -0300

    clase6

---
+++ CLASE6/main.js
@@ -0,0 +1,27 @@
+//CLASE6 - PROMESAS
+
+//Las promesas son un objeto que sigue el estado de una accion.
+//Estados de la accion:
+//1) Pendiente
+//2) Confirmado / Completado
+//3) Rechazado
+
+//L
+
+
+//SINTAXIS
+
+new Promise ((resolve, reject)=>{
+    return "HOLA"
+})
+
+
+
+//setTimeout()
+//Funcion que sirve para retrasar una accion por un tiempo determinado.
+
+//Sintaxis:
+setTimeout(()=>{
+    
+})
+
