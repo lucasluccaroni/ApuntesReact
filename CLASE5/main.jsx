554bb31cb4ddd8e32e8683e4f1c87219e5f83d9e
lucasluccaroni <lucas_luccaroni@hotmail.com.ar>
2023-11-29 07:37:03 -0300

    clase5 min 12

---
+++ CLASE5/main.jsx
@@ -0,0 +1,10 @@
+//CLASE5 - COMPONENTES II
+
+
+//CHIDREN
+//Una manera que tiene React de permitirme pasarle componentes a otro componente, para que se utilicen dentro.
+// Lo podemos usar cuando necesitamos que un componente quede adentro de otro, y que ese componente "que envuelve" pueda luego ser reutilizado.
+//Es como un "marco" de un cuadro, y lo que esta adentro puede cambiar. Puedo utilizarlo en diferentes partes, cambiando el componente que esta en el centro. Ese componente que le paso al componente "marco" se lo va a considerar y va a estar en la propiedad CHILDREN.
+
+//REUTILIZAR EL MARCO Y CAMBIAR EL CENTRO
+
