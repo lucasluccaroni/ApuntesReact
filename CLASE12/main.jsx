166a6d779638a934b41d283a72fb5554188b6092
lucasluccaroni <lucas_luccaroni@hotmail.com.ar>
2023-12-21 00:02:54 -0300

    clase12 firebase

---
+++ CLASE12/main.jsx
@@ -0,0 +1,24 @@
+//SERVICIO DE BACKEND - FIREBASE
+
+//! REPASO DE . MAP Y SPREAD OPERATOR, EJERCITAR JEMPLOS
+
+
+//-FIREBASE 
+//hermano menor de Google Cloud
+//Backend as a service
+
+
+
+//-SDK
+//Conjunto de herramientas para desarrollar software
+//Libreria descargable en nuestro proyecto
+//Nos va a brindar funciones que nosotros vamos a ejecutar para comunicarnos con este backend (para hcer un POST, DELETE, GET, etc.)
+
+//? PROS Y CONTRAS
+/*
+
+
+*/
+
+
+//
